90e23652c5153310d01435d60f669d615fdf6e1c
Daniel Mohl <dmohl@yahoo.com>
2013-04-28 10:21:59 -0500

    Updated the AssemblyInfo for FsUnit

--- src/FsUnit.NUnit/AssemblyInfo.fs
+++ src/FsUnit.NUnit/AssemblyInfo.fs
@@ -4,6 +4,6 @@ open System.Runtime.CompilerServices
 
 [<assembly: AssemblyTitle("FsUnit.NUnit")>]
 [<assembly: AssemblyProduct("FsUnit.NUnit")>]
-[<assembly: AssemblyVersion("1.1.1.0")>]
+[<assembly: AssemblyVersion("1.2.1.0")>]
 [<assembly: InternalsVisibleTo("FsUnit.NUnit.Test")>]
 do()
