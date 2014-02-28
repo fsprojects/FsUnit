7698f92d6e4c406fa1c629caac31f8cfb96074ee
Jay Tuley <jay+code@tuley.name>
2014-02-27 21:30:01 -0600

    Update build.bat
    pause bad for CI

--- build.bat
+++ build.bat
@@ -4,4 +4,3 @@ if not exist packages\FAKE\tools\Fake.exe (
 	".nuget\NuGet.exe" "install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion" "-Prerelease
 )
 "packages\FAKE\tools\Fake.exe" "build.fsx" %*
-pause
