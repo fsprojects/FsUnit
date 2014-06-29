069f48db0a54056fd0fdf00009a9bc695562eafb
Dan Mohl <dmohl@yahoo.com>
2014-06-29 15:01:19 -0500

    Created release 1.3.0.0 of the NUnit, xUnit, and MsTest NuGet packages.

--- src/FsUnit.MsTestUnit/AssemblyInfo.fs
+++ src/FsUnit.MsTestUnit/AssemblyInfo.fs
@@ -4,6 +4,6 @@ open System.Runtime.CompilerServices
 
 [<assembly: AssemblyTitle("FsUnit.MsTest")>]
 [<assembly: AssemblyProduct("FsUnit.MsTest")>]
-[<assembly: AssemblyVersion("1.1.0.0")>]
+[<assembly: AssemblyVersion("1.3.0.0")>]
 [<assembly: InternalsVisibleTo("FsUnit.MsTest.Test")>]
 do()
