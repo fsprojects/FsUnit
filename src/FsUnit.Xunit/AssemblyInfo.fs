1a4a5d455bff4cdd151830c12b370557d52adf8f
Dan Mohl <dmohl@yahoo.com>
2014-06-29 15:26:50 -0500

    Created NuGet packages for release 1.3.0.1

--- src/FsUnit.Xunit/AssemblyInfo.fs
+++ src/FsUnit.Xunit/AssemblyInfo.fs
@@ -4,6 +4,6 @@ open System.Runtime.CompilerServices
 
 [<assembly: AssemblyTitle("FsUnit.Xunit")>]
 [<assembly: AssemblyProduct("FsUnit.Xunit")>]
-[<assembly: AssemblyVersion("1.3.0.0")>]
+[<assembly: AssemblyVersion("1.3.0.1")>]
 [<assembly: InternalsVisibleTo("FsUnit.Xunit.Test")>]
 do()
