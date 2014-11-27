7331f4aa4b456c661888cacd66ff48d763704f83
F# Core Engineering Group (repo management) <fsgit@users.noreply.github.com>
2014-11-27 11:32:21 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ if [ ! -f packages/FAKE/tools/Fake.exe ]; then
   mono --runtime=v4.0 .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion  -Prerelease
 fi
 mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe build.fsx $@
+
