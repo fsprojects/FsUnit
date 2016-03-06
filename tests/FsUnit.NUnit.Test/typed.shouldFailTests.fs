6ecd81dcb794f935e4573328e71835325235b75d
Sergey Tihon <sergey.tihon@gmail.com>
2016-03-07 00:51:47 +0300

    Added test case from #20

--- tests/FsUnit.NUnit.Test/typed.shouldFailTests.fs
+++ tests/FsUnit.NUnit.Test/typed.shouldFailTests.fs
@@ -15,3 +15,7 @@ type ``shouldFail tests`` ()=
     [<Test>] member test.
      ``shouldFail should fail when everything is OK`` ()=
         shouldFail (fun () -> shouldFail id)
+
+    [<Test>] member test.
+     ``Simplify "should throw"``() =
+        (fun () -> failwith "BOOM!") |> shouldFail<System.Exception>
