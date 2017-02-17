d4f69b20558e6c16b0808f3c940e88766030759e
Enrico Sada <enrico@sada.io>
2017-02-17 13:05:36 +0100

    use nunitlite for .netcore testing
    nunit doesnt support yet `dotnet test` and netstandard lib (ref https://github.com/nunit/nunit3-vs-adapter/issues/297 )
    
    Is possible to use nunit lite, ref https://github.com/nunit/docs/wiki/NUnitLite-Runner
    The test lib become a normal console app, and the nunit runner is embeeded (nunitlite.dll), and autostarted at main

---
+++ tests/FsUnit.NUnit.Test/main.fs
@@ -0,0 +1,13 @@
+namespace FsUnit.Test
+
+open NUnit.Framework
+open NUnitLite
+open System.Reflection
+
+type T = { Dummy : string }
+
+module Program =
+
+    [<EntryPoint>]
+    let main args =
+        (new AutoRun(typeof<T>.GetTypeInfo().Assembly)).Execute(args)
