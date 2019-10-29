47eb43d61337dd294d74761905d54eaa5ff6f823
CaptnCodr <constantin.tews@gmail.com>
2019-10-29 21:55:23 +0100

    Add fully readable test case descriptions.

--- tests/FsUnit.MsTest.Test/beSupersetOf.fs
+++ tests/FsUnit.MsTest.Test/beSupersetOf.fs
@@ -6,25 +6,29 @@ open FsUnit.MsTest
 type ``be supersetOf tests`` ()= 
 
     [<TestMethod>] member test.
-     ``list values should be superset of other values`` ()=
+     ``1 to 10 should be superset of 5, 3 and 8`` ()=
         [1..10] |> should be (supersetOf [5;3;8])
 
     [<TestMethod>] member test.
-     ``values should be superset of one value`` ()=
+     ``1 to 10 should be superset of 5`` ()=
         [1..10] |> should be (supersetOf [5])
 
     [<TestMethod>] member test.
-     ``sequence values should be superset of other values`` ()=
+     ``1 to 10 should be superset of 4 to 8`` ()=
         {1..10} |> should be (supersetOf {4..8})
         
     [<TestMethod>] member test.
-     ``array values should be superset of other values`` ()=
+     ``1 to 10 should be superset of 4. 1 and 7`` ()=
         [|1..10|] |> should be (supersetOf [|4;1;7|])
 
     [<TestMethod>] member test.
-     ``values should not be superset of other values`` ()=
+     ``1 to 10 should not be superset of 5, 1 and 11`` ()=
         [1..10] |> should not' (be supersetOf [5;1;11])
 
     [<TestMethod>] member test.
-     ``value should not be superset of values`` ()=
+     ``5 should not be superset of 1 to 10`` ()=
         [5] |> should not' (be supersetOf [1..10])
+
+    [<TestMethod>] member test.
+     ``1 to 10 should be superset of 1 to 10`` ()=
+        [1..10] |> should be (supersetOf [1..10])
