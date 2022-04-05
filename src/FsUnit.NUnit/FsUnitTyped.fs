62fb45e2982466385feafe40eefc13027dbe8f0c
Constantin Tews <constantin.tews@gmail.com>
2022-04-05 12:42:37 +0200

    Fix globbing for formatting.

--- src/FsUnit.NUnit/FsUnitTyped.fs
+++ src/FsUnit.NUnit/FsUnitTyped.fs
@@ -16,7 +16,7 @@ module TopLevelOperators =
 
     [<DebuggerStepThrough>]
     let shouldContain (expected: 'a) (actual: 'a seq) =
-        CollectionAssert.Contains (actual, expected)
+        CollectionAssert.Contains(actual, expected)
 
     [<DebuggerStepThrough>]
     let shouldBeEmpty(actual: 'a seq) =
@@ -24,7 +24,7 @@ module TopLevelOperators =
 
     [<DebuggerStepThrough>]
     let shouldNotContain (expected: 'a) (actual: 'a seq) =
-        CollectionAssert.DoesNotContain (actual, expected, $"Seq %A{actual} should not contain %A{expected}")
+        CollectionAssert.DoesNotContain(actual, expected, $"Seq %A{actual} should not contain %A{expected}")
 
     [<DebuggerStepThrough>]
     let shouldBeSmallerThan (expected: 'a) (actual: 'a) =
