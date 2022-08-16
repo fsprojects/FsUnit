b399874706c74b1042c3e40a4eea217d77062f3b
Constantin Tews <constantin.tews@gmail.com>
2022-08-16 10:28:05 +0200

    Release v5.0.3.

--- src/FsUnit.Xunit/AssemblyInfo.fs
+++ src/FsUnit.Xunit/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsUnit.Xunit")>]
 [<assembly: AssemblyProductAttribute("FsUnit")>]
 [<assembly: AssemblyDescriptionAttribute("FsUnit is a set of libraries that makes unit-testing with F# more enjoyable.")>]
-[<assembly: AssemblyVersionAttribute("5.0.2")>]
-[<assembly: AssemblyFileVersionAttribute("5.0.2")>]
+[<assembly: AssemblyVersionAttribute("5.0.3")>]
+[<assembly: AssemblyFileVersionAttribute("5.0.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FsUnit.Xunit"
     let [<Literal>] AssemblyProduct = "FsUnit"
     let [<Literal>] AssemblyDescription = "FsUnit is a set of libraries that makes unit-testing with F# more enjoyable."
-    let [<Literal>] AssemblyVersion = "5.0.2"
-    let [<Literal>] AssemblyFileVersion = "5.0.2"
+    let [<Literal>] AssemblyVersion = "5.0.3"
+    let [<Literal>] AssemblyFileVersion = "5.0.3"
