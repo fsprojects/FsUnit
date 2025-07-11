bc2d5a586feef694a9917c202485e2042071358b
Constantin Tews <constantin.tews@gmail.com>
2025-07-11 11:10:55 +0200

    Update generated documentation for version 7.1.1.0

--- content/fsdocs-theme-set-dark.js
+++
@@ -1,5 +0,0 @@
-const prefersDark = window.matchMedia("@media (prefers-color-scheme: dark)").matches;
-let currentTheme = localStorage.getItem('theme') ?? (prefersDark ? 'dark' : 'light');
-if (currentTheme === 'dark') {
-    window.document.documentElement.setAttribute("data-theme", 'dark');
-}
